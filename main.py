5233acd3864222ba3b1b7180eb118576139ab069
raznik24 <artem24072008a@gmail.com>
2025-06-05 19:52:48 +0300

    commit

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-from datetime import *
-count = 0
-data = datetime(year=2015, month=1, day=1)
-for i in range (1,13):
-    data = data.replace(month = i)
-    print(data)
-    a = datetime.weekday(data)
-    if a == 0:
-        count += 1    
-data = data.replace(year=2016, month= 1)
-for i in range (1,13):
-    data = data.replace(month = i)
-    print(data)
-    a = datetime.weekday(data)
-    if a == 0:
-        count += 1    
-print(count)
+print('dad')
